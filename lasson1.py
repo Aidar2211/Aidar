e3e10a976681f2f0756b6dc253711615978eadd7
Aidar2211 <livingbeing19@gmail.com>
2020-12-16 19:07:39 +0600

    Second commit

---
+++ lasson1.py
@@ -0,0 +1,17 @@
+print("Hello World!")
+text = "Hello World!!!!!"
+print("Разбуди меня!")
+number = 5678 / 234 * 90- 90
+number2 = 5678
+number3 = "number2"
+
+print(number + 10)
+print(number2 + 10)
+print(number3 * 90)
+number4 = 90
+print(number4 *"Here")
+
+name = input("введите ваше имя")
+age = int(input("введите ваш возраст"))
+print(type(name), type(age))
+
---
+++ lasson1_2.py
@@ -0,0 +1,5 @@
+num1 = int(input("введите 1 число"))
+num2 = int(input("введите 2 число"))
+
+sum = num1 * num2
+print(type(sum))
