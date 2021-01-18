87b58e74e634011f03d31a1caa810f8d83f30715
Aidar2211 <livingbeing19@gmail.com>
2021-01-18 09:47:22 +0600

    dz

--- tosvt.py
+++ tosvt.py
@@ -2,55 +2,65 @@ import random
 
 print("Игра началась!")
 print("Камень, Ножницы, Бумага")
-aa = random.randint(1, 3)
-if aa == 1:
+computer = random.randint(1, 3)
+if computer == 1:
     a = "Камень"
-elif aa == 2:
+elif computer == 2:
     a = "Ножницы"
 else:
     a = "Бумага"
 print("1 Камень")
 print("2 Ножницы")
 print("3 Бумага")
-try:
-    n = int(input("Написат номер: "))
-    if n == 1:
-        print("")
-        print("Ваш выбор: Камень")
-        print("Выбор соперника: " + str(a))
-        print("")
-        if aa == 1:
-            print("Ничья:")
-        elif aa == 2:
-            print("Вы победили!")
-        else:
-            print("Комп победил \n Вы проиграли!")
+
+
+def game():
+    try:
+        n = int(input("Написать номер: "))
+        if n == 1:
             print("")
-    elif n == 2:
-        print("")
-        print("Ваш выбор: Ножницы")
-        print("Выбор соперника: " + str(a))
-        print("")
-        if aa == 1:
-            print("Победа соперника\n Вы проиграли!")
-        elif aa == 2:
-            print("Ничья")
-        else:
-            print("Вы победили!")
+            print("Ваш выбор: Камень")
+            print("Выбор соперника: " + str(a))
             print("")
-    elif n == 3:
-        print("")
-        print("Ваш выбор: бумага")
-        print("Выбор соперника " + str(a))
-        print("")
-        if aa == 1:
-            print("Вы победили!")
-        elif aa == 2:
-            print("Комп победил\n Вы проиграли!")
+            if computer == 1:
+                print("Ничья:")
+            elif computer == 2:
+                print("Вы победили!")
+            else:
+                print("Комп победил \n Вы проиграли!")
+                print("")
+        elif n == 2:
+            print("")
+            print("Ваш выбор: Ножницы")
+            print("Выбор соперника: " + str(a))
+            print("")
+            if computer == 1:
+                print("Победа соперника\n Вы проиграли!")
+            elif computer == 2:
+                print("Ничья")
+            else:
+                print("Вы победили!")
+                print("")
+        elif n == 3:
+            print("")
+            print("Ваш выбор: бумага")
+            print("Выбор соперника " + str(a))
+            print("")
+            if computer == 1:
+                print("Вы победили!")
+            elif computer == 2:
+                print("Комп победил\n Вы проиграли!")
+            else:
+                print("Ничья")
+            print("")
         else:
-            print("Ничья")
-        print("")
-    else:
-        print("Пишите только числа: 1/2/3!")
-except ValueError:
-    print("ОШИБКА: записывать только числа!")
+            print("Пишите только числа: 1/2/3!")
+    except ValueError:
+        print("ОШИБКА: записывать только числа!")
+
+    restart = input("Do want to play Again?(y/n)")
+    if restart == "y" or restart == "Y":
+        game()
+
+
+game()
