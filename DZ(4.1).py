3a2e72b1fc950a3986de1ad3c33adc7651964c27
Aidar2211 <76105239+Aidar2211@users.noreply.github.com>
2021-02-17 19:09:46 +0600

    Add files via upload

--- DZ(4.1).py
+++ DZ(4.1).py
@@ -1,33 +1,15 @@
-class Sort:
-    def __init__(self):
-        self.list = [34, 12, 7, 92, 5, 18, 4, 32, 48, 11, 74]
-        self.pivot = self.list[6]
-        self.left = list(filter(lambda x: x < self.pivot, self.list))
-        self.center = [x for x in self.list if x == self.pivot]
-        self.right = []
+inp = open ('input.txt')
+fnp = open('output.txt', 'w')
+read = inp.read().split()
+a = list(map(int, read))[1:]
+l = [0 for _ in range(-100, 101)]
+for x in range(len(a)):
+    num = a[x]
+    l[num] += 1
 
-    def bubble_sort(self):
-        for j in range(len(self.list)):
-            swapped = False
-            i = 0
-            while i < len(self.list) - 1:
-                if self.list[i] > self.list[i + 1]:
-                    self.list[i], self.list[i + 1] = self.list[i + 1], self.list[i]
-                    swapped = True
-                i = i + 1
-            if not swapped:
-                break
-        print(self.list)
-
-    def quick_sort(self):
-        if len(self.list) <= 1:
-            return self.list
-        for x in self.list:
-            if x > self.pivot:
-                self.right.append(x)
-        return self.left + self.center + self.right
-
-
-sort = Sort()
-sort.bubble_sort()
-print(sort.quick_sort())
+new = []
+s = ''
+for m in range(-100, 101):
+    if l[m]:
+        s += ((str( m ))+ ' ') * l[m]
+fnp.write(s)
